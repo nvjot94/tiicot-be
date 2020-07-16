d55545223fac73ee667f89493b96f4a3a49ce2c8
nvjot94 <nvjot94@gmail.com>
2020-07-16 16:35:41 -0400

    index.html file

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ app.get('/users', async (req, res) => {
       res.status(500).send('Server Error');
     }
   });
-
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('./public'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  );
+}
 app.listen(process.env.PORT||8888,()=>{
 console.log('server has been started');
 });
